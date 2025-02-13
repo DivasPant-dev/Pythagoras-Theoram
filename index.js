bb85aca88b27dd894f277f67ed467fcf1c15e98e
DivasPant-dev <divaspant03@gmail.com>
2025-02-13 18:17:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let input2 = document.getElementById("input2");
 let display = document.getElementById("display");
 
 function check() {
-    if (input1.value == "" || input2.value == "") {
+    if (input1.value === "" || input2.value === "") {
         display.value = "Error";
-    }
-    else {
-        input1 = input1.value;
-        input2 = input2.value;
+    } else {
+        let value1 = parseFloat(input1.value);
+        let value2 = parseFloat(input2.value);
 
-        const result = Math.sqrt(Math.pow(input1, 2) + Math.pow(input2, 2));
+        const result = Math.sqrt(Math.pow(value1, 2) + Math.pow(value2, 2));
 
-        let displayAccess = document.getElementById("display");
+        display.value = "Hypotenuse = " + result;
 
-        displayAccess.value += "Hypotenuse = " + result;
+        // Clear the input values
+        input1.value = "";
+        input2.value = "";
     }
 }
 
